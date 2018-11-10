5c479dc9c732e1fa7afc180a236fab678917c1e4
Gil Barbara <gilbarbara@gmail.com>
2018-11-10 20:27:24 -0200

    Add tests

---
+++ test/ConnectedRouter.spec.js
@@ -0,0 +1,113 @@
+/* eslint-disable import/no-named-default */
+import { mount, default as Enzyme } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route } from 'react-router';
+import {
+  applyMiddleware, createStore, compose, combineReducers,
+} from 'redux';
+import { createBrowserHistory } from 'history';
+
+import * as actions from '../src/actions';
+import {
+  connectRouter, routerMiddleware, ConnectedRouter, LOCATION_CHANGE,
+} from '../src';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('ConnectedRouter', () => {
+  let history;
+  let onLocationChangedSpy;
+  let store;
+  let wrapper;
+
+  beforeAll(() => {
+    onLocationChangedSpy = jest.spyOn(actions, 'onLocationChanged');
+
+    history = createBrowserHistory();
+
+    store = createStore(
+      combineReducers({
+        router: connectRouter(history),
+      }), // root reducer with router state
+      {},
+      compose(
+        applyMiddleware(
+          routerMiddleware(history),
+        ),
+      ),
+    );
+
+    wrapper = mount(
+      <ConnectedRouter history={history}>
+        <Route path="/" render={() => <div>Home</div>} />
+      </ConnectedRouter>,
+      {
+        context: {
+          store,
+        },
+        childContextTypes: {
+          store: PropTypes.object,
+        },
+      },
+    );
+  });
+
+  it('calls `props.onLocationChanged()` on mount', () => {
+    expect(onLocationChangedSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls `props.onLocationChanged()` when pathname changes.', () => {
+    history.push('/one');
+
+    expect(onLocationChangedSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it('calls `props.onLocationChanged()` when search changes.', () => {
+    history.push({ search: '?cache=11122' });
+
+    expect(store.getState().router.location.search).toBe('?cache=11122');
+    expect(onLocationChangedSpy).toHaveBeenCalledTimes(3);
+  });
+
+  it('calls `props.onLocationChanged()` when hash changes.', () => {
+    history.push({ hash: 'profile' });
+
+    expect(store.getState().router.location.hash).toBe('#profile');
+    expect(onLocationChangedSpy).toHaveBeenCalledTimes(4);
+  });
+
+  it('calls `props.onLocationChanged()` when state changes.', () => {
+    history.push({ state: { modal: true } });
+
+    expect(store.getState().router.location.state).toEqual({ modal: true });
+    expect(onLocationChangedSpy).toHaveBeenCalledTimes(5);
+  });
+
+  it('calls `history.push()` when the store location differs from history', () => {
+    expect(history.location.pathname).toBe('/one');
+
+    store.dispatch({
+      type: LOCATION_CHANGE,
+      payload: {
+        location: {
+          ...store.getState().router.location,
+          pathname: '/two',
+        },
+      },
+    });
+
+    expect(history.location.pathname).toBe('/two');
+    expect(onLocationChangedSpy).toHaveBeenCalledTimes(5);
+  });
+
+  it('unlistens the history object when unmounted.', () => {
+    wrapper.unmount();
+
+    history.push('/new-location-after-unmounted');
+
+    expect(onLocationChangedSpy).toHaveBeenCalledTimes(5);
+  });
+});
---
+++ test/reducer.spec.js
@@ -0,0 +1,86 @@
+import { combineReducers } from 'redux';
+import { LOCATION_CHANGE, connectRouter } from '../src';
+
+describe('connectRouter', () => {
+  let mockHistory;
+
+  beforeEach(() => {
+    mockHistory = {
+      location: {
+        pathname: '/',
+        search: '',
+        hash: '',
+      },
+      action: 'POP',
+    };
+  });
+
+  it('creates new root reducer with router reducer inside', () => {
+    const mockReducer = (state = {}, action) => {
+      switch (action.type) {
+        default:
+          return state;
+      }
+    };
+    const rootReducer = combineReducers({
+      mock: mockReducer,
+      router: connectRouter(mockHistory),
+    });
+
+    const currentState = {
+      mock: {},
+      router: {
+        location: {
+          pathname: '/',
+          search: '',
+          hash: '',
+        },
+        action: 'POP',
+      },
+    };
+    const action = {
+      type: LOCATION_CHANGE,
+      payload: {
+        location: {
+          pathname: '/path/to/somewhere',
+          search: '?query=test',
+          hash: '',
+        },
+        action: 'PUSH',
+      },
+    };
+    const nextState = rootReducer(currentState, action);
+    const expectedState = {
+      mock: {},
+      router: {
+        location: {
+          pathname: '/path/to/somewhere',
+          search: '?query=test',
+          hash: '',
+        },
+        action: 'PUSH',
+      },
+    };
+    expect(nextState).toEqual(expectedState);
+  });
+
+  it('does not change state ref when action does not trigger any reducers', () => {
+    const rootReducer = combineReducers({
+      router: connectRouter(mockHistory),
+    });
+
+    const currentState = {
+      router: {
+        location: {
+          pathname: '/',
+          search: '',
+          hash: '',
+        },
+        action: 'POP',
+      },
+    };
+    const nextState = rootReducer(currentState);
+
+    expect(nextState).toBe(currentState);
+  });
+});
