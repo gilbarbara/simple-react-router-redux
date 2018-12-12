34660cee87c6467f77183e6f45817bef47d3f09a
Gil Barbara <gilbarbara@gmail.com>
2018-12-12 13:23:31 -0200

    Upgrade react-redux to 6.x

--- src/ConnectedRouter.js
+++ src/ConnectedRouter.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Router } from 'react-router';
-import { connect } from 'react-redux';
+import { connect, ReactReduxContext } from 'react-redux';
 
 import { onLocationChanged } from './actions';
 
 class ConnectedRouter extends React.Component {
-  constructor(props, context) {
+  constructor(props) {
     super(props);
-    const { history, onLocationChanged: onChanged } = this.props;
+    const { history, onLocationChanged: onChanged, store } = this.props;
 
     this.inTimeTravelling = false;
-    this.unsubscribe = context.store.subscribe(() => {
+    this.unsubscribe = store.subscribe(() => {
       // Extract store's location
       const {
         pathname: pathnameInStore,
         search: searchInStore,
         hash: hashInStore,
-      } = context.store.getState().router.location;
+      } = store.getState().router.location;
       // Extract history's location
       const {
         pathname: pathnameInHistory,
@@ -63,9 +63,6 @@ class ConnectedRouter extends React.Component {
       push: PropTypes.func.isRequired,
     }).isRequired,
     onLocationChanged: PropTypes.func.isRequired,
-  };
-
-  static contextTypes = {
     store: PropTypes.shape({
       getState: PropTypes.func.isRequired,
       subscribe: PropTypes.func.isRequired,
@@ -94,7 +91,27 @@ const mapDispatchToProps = dispatch => ({
     dispatch(onLocationChanged(location, action)),
 });
 
+const ConnectedRouterWithContext = props => {
+  const { context } = props;
+  const Context = context || ReactReduxContext;
+
+  /* istanbul ignore next */
+  if (Context == null) {
+    throw new Error('Please upgrade to react-redux v6');
+  }
+
+  return (
+    <Context.Consumer>
+      {({ store }) => <ConnectedRouter store={store} {...props} />}
+    </Context.Consumer>
+  );
+};
+
+ConnectedRouterWithContext.propTypes = {
+  context: PropTypes.object,
+};
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(ConnectedRouter);
+)(ConnectedRouterWithContext);
--- test/ConnectedRouter.spec.js
+++ test/ConnectedRouter.spec.js
@@ -1,10 +1,11 @@
-/* eslint-disable import/no-named-default */
-import { mount, default as Enzyme } from 'enzyme';
+/* eslint-disable import/no-named-default, react/no-unused-state */
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route } from 'react-router';
+import { ReactReduxContext } from 'react-redux';
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import { createBrowserHistory } from 'history';
 
@@ -16,7 +17,35 @@ import {
   LOCATION_CHANGE,
 } from '../src';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
+
+class MockProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    const { store } = props;
+
+    this.state = {
+      storeState: store.getState(),
+      store,
+    };
+  }
+
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    store: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const { children } = this.props;
+    const Context = ReactReduxContext;
+
+    return (
+      <Context.Provider value={this.state}>
+        {React.Children.only(children)}
+      </Context.Provider>
+    );
+  }
+}
 
 describe('ConnectedRouter', () => {
   let history;
@@ -38,49 +67,45 @@ describe('ConnectedRouter', () => {
     );
 
     wrapper = mount(
-      <ConnectedRouter history={history}>
-        <Route path="/" render={() => <div>Home</div>} />
-      </ConnectedRouter>,
-      {
-        context: {
-          store,
-        },
-        childContextTypes: {
-          store: PropTypes.object,
-        },
-      },
+      <MockProvider store={store}>
+        <ConnectedRouter history={history}>
+          <Route path="/" render={() => <div>Home</div>} />
+        </ConnectedRouter>
+      </MockProvider>,
     );
   });
 
-  it('calls `props.onLocationChanged()` on mount', () => {
-    expect(onLocationChangedSpy).toHaveBeenCalledTimes(1);
-  });
+  describe('props.onLocationChanged', () => {
+    it('calls it on mount', () => {
+      expect(onLocationChangedSpy).toHaveBeenCalledTimes(1);
+    });
 
-  it('calls `props.onLocationChanged()` when pathname changes.', () => {
-    history.push('/one');
+    it('calls it when pathname changes.', () => {
+      history.push('/one');
 
-    expect(onLocationChangedSpy).toHaveBeenCalledTimes(2);
-  });
+      expect(onLocationChangedSpy).toHaveBeenCalledTimes(2);
+    });
 
-  it('calls `props.onLocationChanged()` when search changes.', () => {
-    history.push({ search: '?cache=11122' });
+    it('calls it when search changes.', () => {
+      history.push({ search: '?cache=11122' });
 
-    expect(store.getState().router.location.search).toBe('?cache=11122');
-    expect(onLocationChangedSpy).toHaveBeenCalledTimes(3);
-  });
+      expect(store.getState().router.location.search).toBe('?cache=11122');
+      expect(onLocationChangedSpy).toHaveBeenCalledTimes(3);
+    });
 
-  it('calls `props.onLocationChanged()` when hash changes.', () => {
-    history.push({ hash: 'profile' });
+    it('calls it when hash changes.', () => {
+      history.push({ hash: 'profile' });
 
-    expect(store.getState().router.location.hash).toBe('#profile');
-    expect(onLocationChangedSpy).toHaveBeenCalledTimes(4);
-  });
+      expect(store.getState().router.location.hash).toBe('#profile');
+      expect(onLocationChangedSpy).toHaveBeenCalledTimes(4);
+    });
 
-  it('calls `props.onLocationChanged()` when state changes.', () => {
-    history.push({ state: { modal: true } });
+    it('calls it when state changes.', () => {
+      history.push({ state: { modal: true } });
 
-    expect(store.getState().router.location.state).toEqual({ modal: true });
-    expect(onLocationChangedSpy).toHaveBeenCalledTimes(5);
+      expect(store.getState().router.location.state).toEqual({ modal: true });
+      expect(onLocationChangedSpy).toHaveBeenCalledTimes(5);
+    });
   });
 
   it('calls `history.push()` when the store location differs from history', () => {
